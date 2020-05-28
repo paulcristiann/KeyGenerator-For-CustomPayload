3ae5400de3c91c59b61c2150b76b1a1a54aeeac9
Paul-Cristian Vasile <paul.cristian.vasile04@gail.com>
2020-05-28 11:54:20 +0300

    Add project files.

---
+++ KeyGenerator/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace KeyGenerator
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void generate_key(object sender, EventArgs e)
+        {
+            if(id_entry.Text == null || app_version.Text == null)
+            {
+                DisplayAlert("Info", "Please enter the ID and app version", "Ok");
+                return;
+            }
+            else
+            {
+                var id = id_entry.Text;
+                var appVersion = app_version.Text;
+                generated_entry.Text = GetMd5Sum(id + appVersion);
+            }
+        }
+
+        static string GetMd5Sum(string productIdentifier)
+        {
+            System.Text.Encoder enc = System.Text.Encoding.Unicode.GetEncoder();
+            byte[] unicodeText = new byte[productIdentifier.Length * 2];
+            enc.GetBytes(productIdentifier.ToCharArray(), 0, productIdentifier.Length, unicodeText, 0, true);
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] result = md5.ComputeHash(unicodeText);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                sb.Append(result[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+
+        static string FormatLicenseKey(string productIdentifier)
+        {
+            productIdentifier = productIdentifier.Substring(0, 28).ToUpper();
+            char[] serialArray = productIdentifier.ToCharArray();
+            StringBuilder licenseKey = new StringBuilder();
+
+            int j = 0;
+            for (int i = 0; i < 28; i++)
+            {
+                for (j = i; j < 4 + i; j++)
+                {
+                    licenseKey.Append(serialArray[j]);
+                }
+                if (j == 28)
+                {
+                    break;
+                }
+                else
+                {
+                    i = (j) - 1;
+                    licenseKey.Append("-");
+                }
+            }
+            return licenseKey.ToString();
+        }
+    }
+}
